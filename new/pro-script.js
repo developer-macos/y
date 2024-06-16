d969e2d7a0f9525269b0dbc036086d74492390f5
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2024-06-16 11:59:30 +0300

    h

---
+++ new/pro-script.js
@@ -0,0 +1,24 @@
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (name && email && message) {
+      alert("Thank you for your message!");
+    } else {
+      alert("Please fill in all fields.");
+    }
+  });
+
+// Smooth scrolling
+document.querySelectorAll("nav ul li a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
